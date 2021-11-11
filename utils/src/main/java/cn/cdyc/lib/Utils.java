fdbd1decac058d9260ceebddb6b04bc8c304d339
Yi Hou <eonhy@hotmail.com>
2021-11-11 10:07:35 +0800

    Pass

---
+++ utils/src/main/java/cn/cdyc/lib/Utils.java
@@ -0,0 +1,13 @@
+package cn.cdyc.lib;
+
+public class Utils {
+    static int num_test=99;
+    static int value_test=29;
+    static String test(){
+        String company_name ="CBPC";
+        return company_name;
+    }
+
+}
+
+
